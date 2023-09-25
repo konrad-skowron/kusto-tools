b01a5d3ca2f7b30e8a174b1f941094fc6a765472
Konrad Skowron <73618275+Konrad-Skowron@users.noreply.github.com>
2023-09-25 20:09:37 +0200

    new feature

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-function slashMapping(event) {
-    const mapping = input.value;
+let columnIndex = 0;
+const typeArr = ['bool', 'datetime', 'dynamic', 'guid', 'int', 'long', 'real', 'string', 'timespan', 'decimal', 'custom'];
+const columnArr = [];
+let columnStringArr = [];
+
+const masterContainer = document.getElementById('container');
+const container = document.getElementById('generatorContainer');
+const form = document.getElementById('mappingForm');
+const input = document.getElementById('mappingInput');
+const resutl = document.getElementById('result');
+
+form.addEventListener('submit', (event) => {
+    const slashedMapping = slashMapping();
+    printMapping(slashedMapping);
+    event.preventDefault();
+});
+
+function Column(id) {
+    this.id = id;
+    this.parent = null;
+    this.children = [];
+}
+
+function slashMapping(mappingArg) {
+    const mapping = mappingArg || input.value;
     const mappingArr = [];
     let stackSize = 0;
 
@@ -28,14 +51,276 @@ function slashMapping(event) {
 
     input.value = '';
     if (stackSize !== 0) {
-        resutl.textContent  = 'Error! Check your input and try again.'
+        return 'Error! Check your input and try again.';
     } else {
-        resutl.textContent  = mappingArr.join('');
+        return mappingArr.join('');
     }
-    event.preventDefault(); // prevent page reload on form submit
 }
 
-const form = document.getElementById('mappingForm');
-const input = document.getElementById('mappingInput');
-const resutl = document.getElementById('result');
-form.addEventListener("submit", slashMapping);
+function printMapping(slashedMapping) {
+    resutl.textContent = slashedMapping;
+}
+
+function addColumn() {
+    columnArr.push(new Column(columnIndex));
+    displayGenerator(container);
+    columnIndex++;
+}
+
+function displayGenerator(targetDiv) {
+    const formDiv = document.createElement('div');
+    formDiv.id = 'formDiv' + columnIndex;
+    formDiv.style.marginLeft = '30px';
+
+    const form = document.createElement('form');
+    form.id = 'form' + columnIndex;
+    form.classList.add('d-inline-flex');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+    });
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Column';
+
+    const select = document.createElement('div');
+    select.classList.add('dropdown');
+    const dropdownBtn = document.createElement('button');
+    dropdownBtn.classList.add('btn', 'btn-primary', 'btn-sm', 'dropdown-toggle');
+    dropdownBtn.setAttribute('aria-expanded', 'false');
+    dropdownBtn.setAttribute('data-bs-toggle', 'dropdown');
+    dropdownBtn.setAttribute('type', 'button');
+    dropdownBtn.textContent = 'type';
+    dropdownBtn.style.fontStyle = 'italic';
+    const dropdownMenu = document.createElement('div');
+    dropdownMenu.classList.add('dropdown-menu');
+    for (let j = 0; j < typeArr.length; j++) {
+        const dropdownItem = document.createElement('li');
+        const dropdownContent = document.createElement('a');
+        dropdownContent.classList.add('dropdown-item');
+        dropdownContent.text = typeArr[j];
+        dropdownItem.addEventListener('click', () => {
+            dropdownBtn.style.fontStyle = 'normal';
+            dropdownBtn.textContent = dropdownContent.text;
+        });
+        dropdownItem.appendChild(dropdownContent);
+        dropdownMenu.appendChild(dropdownItem);
+    }
+    select.appendChild(dropdownBtn);
+    select.appendChild(dropdownMenu);
+    
+    const addSubColumnBtn = document.createElement('button');
+    addSubColumnBtn.id = columnIndex;
+    addSubColumnBtn.textContent = 'Add subcolumn';
+    addSubColumnBtn.classList.add('btn', 'btn-primary', 'btn-sm');
+    addSubColumnBtn.setAttribute('type', 'button');
+    addSubColumnBtn.addEventListener('click', () => {
+        const formDiv = document.getElementById('formDiv' + addSubColumnBtn.id);
+        const newColumn = new Column(columnIndex);
+        let i = 0;
+        for (const column of columnArr) {
+            if (column.id == addSubColumnBtn.id) {
+                newColumn.parent = column;
+                column.children.push(newColumn);
+                break;
+            }
+            i++;
+        }
+        columnArr.splice(i++, 0, newColumn);
+        displayGenerator(formDiv);
+        columnIndex++;
+    });
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.id = columnIndex;
+    deleteBtn.textContent = '⨉';
+    deleteBtn.classList.add('btn', 'btn-danger', 'btn-sm');
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.title = 'Experimental';
+    deleteBtn.addEventListener('click', () => {
+        const formDiv = document.getElementById('formDiv' + deleteBtn.id);
+        formDiv.parentNode.removeChild(formDiv);
+        let parentId;
+        for (const column of columnArr) {
+            if (column.id == deleteBtn.id) {
+                if (column.parent == null) {
+                    return;
+                }
+                parentId = column.parent.id;
+            }
+        }
+        for (const column of columnArr) {
+            if (column.id == parentId) {
+                for (let i = 0; i < column.children.length; i++) {
+                    if (column.children[i].id == deleteBtn.id) {
+                        column.children.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    });
+
+    form.appendChild(input);
+    form.appendChild(select);
+    form.appendChild(addSubColumnBtn);
+    form.appendChild(deleteBtn);
+    formDiv.appendChild(form);
+    targetDiv.appendChild(formDiv);
+}
+
+function generateConfig() {
+    columnStringArr = [];
+    let config = '';
+    const tableConfig = document.getElementById('tableConfig');
+    const tableNameInput = document.querySelector('input');
+    if (tableNameInput.value === '') {
+        tableConfig.style.color = '#dc3545';
+        tableConfig.textContent = 'Error! Specify table name.'
+        return;
+    }
+    for (const column of columnArr) {
+        if (column.parent == null && column.children.length === 0) {
+            try {
+                const form = document.getElementById('form' + column.id);
+                const input = form.querySelector('input');
+                const dropdown = form.querySelector('div.dropdown');
+                const dropdownBtn = dropdown.querySelector('button');
+                if (dropdownBtn.textContent === 'type' || input.value === '') {
+                    tableConfig.style.color = '#dc3545';
+                    tableConfig.textContent = 'Error! All columns must have specified name and type.'
+                    return;
+                }
+                config += input.value + ': ' + dropdownBtn.textContent + ', ';
+                columnStringArr.push(input.value);
+            } catch (error) {
+                continue;
+            }
+        } else if (column.children.length === 0) {
+            let parentPath = '';
+            let tempColumn = column;
+            while (tempColumn != null) {
+                try {
+                    const form = document.getElementById('form' + tempColumn.id);
+                    const input = form.querySelector('input');
+                    const dropdown = form.querySelector('div.dropdown');
+                    const dropdownBtn = dropdown.querySelector('button');
+                    if (dropdownBtn.textContent === 'type' || input.value === '') {
+                        tableConfig.style.color = '#dc3545';
+                        tableConfig.textContent = 'Error! All columns must have specified name and type.'
+                        return;
+                    }
+                    parentPath = input.value + '_' + parentPath;
+                    tempColumn = tempColumn.parent;
+                } catch (error) {
+                    parentPath = '';
+                    break;
+                }
+            }
+            if (parentPath !== '') {
+                config += parentPath.slice(0, -1) + ': ' + dropdownBtn.textContent + ', ';
+                columnStringArr.push(parentPath.slice(0, -1));
+            }
+        }
+    }
+    if (config === '') {
+        tableConfig.style.color = '#dc3545';
+        tableConfig.textContent = 'Error! Add at least one column.';
+        return;
+    }
+    addEnhancedColumns();
+    printConfig(config, tableNameInput.value);
+}
+
+function addEnhancedColumns() {
+    columnStringArr.push('_eventType');
+    columnStringArr.push('_kafkaTopic');
+    columnStringArr.push('_kafkaPartition');
+    columnStringArr.push('_kafkaOffset');
+    columnStringArr.push('_kafkaTimestamp');
+    columnStringArr.push('_schema');
+    columnStringArr.push('_eventTime');
+    columnStringArr.push('_indexTime');
+    columnStringArr.push('_aggregateId');
+    columnStringArr.push('_correlationId');
+    columnStringArr.push('_headers_name');
+    columnStringArr.push('_headers_value');
+}
+
+function printConfig(config, tableName) {
+    let mappingScript = '[';
+    for (const column of columnStringArr) {
+        let pathString = '$';
+        if (column[0] === '_') {
+            let tempColumn = '@' + column.slice(1);
+            pathString += '[\'' + tempColumn +'\']'
+        } else {
+            let path = column.split('_');
+            for (const word of path) {
+                pathString += '[\'' + word +'\']'
+            }
+        }
+        mappingScript += 
+        '{' +
+            '"column":"' + column + '",' +
+            '"path":"' + pathString + '",' +
+            '"datatype": "",' +
+            '"transform": ""'
+        '},';
+    }
+    mappingScript = mappingScript.slice(0, -1) + ']';
+    const slashedMappingScript = slashMapping(mappingScript);
+
+    const tableConfig = document.getElementById('tableConfig');
+    tableConfig.style.color = '#dee2e6';
+    tableConfig.innerHTML = 
+    '<pre>' +
+    '{ <br>' +    
+    '   id: \'\', <br>' +
+    '   script: \'.create table ' + tableName + ' (' + config.slice(0, -2) + ')\', <br>' +
+    '   author: \'\', <br>' +
+    '   comment: \'\', <br>' +
+    '   env: \'\', <br>' +
+    '}, <br>' +
+    '<br>' +
+    '{ <br>' +
+    '   id: \'\', <br>' +
+    '   script: \'.create table ' + tableName + ' ingestion json mapping \\"' + tableName + '_mapping\\" ' + slashedMappingScript + '\', <br>' +
+    '   author: \'\', <br>' +
+    '   comment: \'\', <br>' +
+    '   env: \'\', <br>' +
+    '} <br>' +
+    '</pre>';
+    
+}
+
+function init() {
+    const form = document.createElement('form');
+    form.classList.add('d-inline-flex');
+    form.addEventListener('submit', (event) => {
+        generateConfig();
+        event.preventDefault();
+    });
+    const tableNameInput = document.createElement('input');
+    tableNameInput.type = 'text';
+    tableNameInput.placeholder = 'Table name';
+
+    const addColumnBtn = document.createElement('button');
+    addColumnBtn.textContent = 'Add column';
+    addColumnBtn.classList.add('btn', 'btn-primary', 'btn-sm');
+    addColumnBtn.setAttribute('type', 'button');
+    addColumnBtn.addEventListener('click', addColumn);
+
+    const submitBtn = document.createElement('button');
+    submitBtn.textContent = 'Submit';
+    submitBtn.classList.add('btn', 'btn-success', 'btn-sm');
+    submitBtn.setAttribute('type', 'button');
+    submitBtn.addEventListener('click', generateConfig);
+
+    form.appendChild(tableNameInput);
+    form.appendChild(addColumnBtn);
+    form.appendChild(submitBtn);
+    container.appendChild(form);
+}
+
+init();
